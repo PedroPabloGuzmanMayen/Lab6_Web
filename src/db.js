d9dab3d141a28ef1f4d5738a571a03038d053658
PedroPabloGuzmanMayen <ppguzmanmayen@gmail.com>
2024-03-27 16:40:50 -0600

    Fix: lint errors fixed

--- src/db.js
+++ src/db.js
@@ -1,6 +1,10 @@
-import conn from './conn.js'
+import conn from './conn'
 
 export async function getPosts() {
-    const [rows] = await conn.query('SELECT * FROM blog_posts')
-    return rows
+  const [rows] = await conn.query('SELECT * FROM blog_starwars')
+  return rows
+}
+
+export function modifyPostByID(id, title, content) {
+  return conn.query('UPDATE blog_starwars SET title = ?, content = ? WHERE id = ?', [title, content, id])
 }
--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import {getPosts} from './db.js'
+import { getPosts } from './db'
 
 const app = express()
 const port = 22111
 app.get('/', async (req, res) => {
+  console.log('All post: ', await getPosts())
   res.send('Hello World!')
 })
 app.listen(port, () => {
