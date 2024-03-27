4b3ed479bbd71fb7f33053155597aa9a3a5e172d
PedroPabloGuzmanMayen <ppguzmanmayen@gmail.com>
2024-03-27 16:56:22 -0600

    Feat: database connection errors

--- src/db.js
+++ src/db.js
@@ -1,10 +1,10 @@
 import conn from './conn'
 
 export async function getPosts() {
-  const [rows] = await conn.query('SELECT * FROM blog_starwars')
+  const [rows] = await conn.query('SELECT * FROM blog_posts')
   return rows
 }
 
 export function modifyPostByID(id, title, content) {
-  return conn.query('UPDATE blog_starwars SET title = ?, content = ? WHERE id = ?', [title, content, id])
+  return conn.query('UPDATE blog_posts SET title = ?, content = ? WHERE id = ?', [title, content, id])
 }
