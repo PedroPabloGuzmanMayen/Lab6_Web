dfa07b8ca1a027e7204700f890bb256baf449ac9
Juan Pablo Guzmán <juanpabloguzman@MacBook-Air-de-Juan-2.local>
2024-03-30 23:44:39 -0600

    fix: post method errors

--- src/db.js
+++ src/db.js
@@ -1,17 +1,34 @@
 import conn from './conn.js'
 
+// Con esta función se obtienen todos los posts de la base de datos
 export async function getPosts() {
   const [rows] = await conn.query('SELECT * FROM blog_posts')
   return rows
 }
+// Con esta func
+export async function modifyPostByID(id, title, content) {
+  if (title === '') {
+    return conn.query('UPDATE blog_posts SET content = ? WHERE id = ?', [content, id])
+  }
+  if (content === '') {
+    return conn.query('UPDATE blog_posts SET title = ? WHERE id = ?', [title, id])
+  }
 
-export function modifyPostByID(id, title, content) {
   return conn.query('UPDATE blog_posts SET title = ?, content = ? WHERE id = ?', [title, content, id])
 }
 
-export function newPost(title, content, image){
-  if (image === undefined){
+export async function newPost(title, content, image) {
+  if (image === '') {
     return conn.query('INSERT INTO blog_posts (title, content) VALUES (?, ?)', [title, content])
   }
+
   return conn.query('INSERT INTO blog_posts (title, content, banner) VALUES (?, ?, ?)', [title, content, image])
+}
+
+export async function getPostbyID(id) {
+  return conn.query('SELECT * FROM blog_posts WHERE id = ?', [id])
+}
+
+export async function deletePost(id) {
+  return conn.query('DELETE FROM blog_posts WHERE id = ?', [id])
 }
--- src/main.js
+++ src/main.js
@@ -3,20 +3,25 @@ import { getPosts } from './db.js'
 
 const app = express()
 const port = 22111
+app.use(express.json())
 app.get('/', (req, res) => {
   res.send('Hello from a galaxy far far away!')
 })
 app.get('/Posts', async (req, res) => {
   try {
     res.status(200).json(await getPosts())
-  }
-  catch (e) {
+  } catch (e) {
     res.status(500).send('Ha ocurrido un error, el lado oscuro ha triunfado :( (*suena la marcha imperial*)')
   }
 })
-app.post('/newPost', (req, res) => {
-  
-
+app.post('/newPost', async (req, res) => {
+  try {
+    const { title, content, image } = req.body
+    await newPost(title, content, image)
+    res.status(200).send('Post creado correctamente :) (*suena la canción de throne room [escena de la entrega de medallas en el ep. 4]*)')
+  } catch(e) {
+    res.status(500).send('Ha ocurrido un error, el lado oscuro ha triunfado :( (*suena la marcha imperial*)')
+  }
 })
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
