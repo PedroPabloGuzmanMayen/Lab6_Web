7e4117c1abd041b2226cf8d17a3a7e12093d0147
PedroPabloGuzmanMayen <ppguzmanmayen@gmail.com>
2024-04-03 13:59:23 -0600

    Fix: endpoint validation fix

--- src/main.js
+++ src/main.js
@@ -6,6 +6,9 @@ import {
 const app = express()
 const port = 22111
 app.use(express.json())
+app.use((req, res, next) => {
+  res.status(404).send('404 Not Found');
+})
 app.get('/', (req, res) => {
   res.send('Hello from a galaxy far far away!')
 })
@@ -47,9 +50,7 @@ app.put('/modifyPost/:id', async (req, res) => {
     res.status(500).send('Error :(')
   }
 })
-app.use((req, res, next) => {
-  res.status(404).send('404 Not Found');
-})
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
