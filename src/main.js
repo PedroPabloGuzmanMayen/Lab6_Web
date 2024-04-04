1e7fa290d552f1b6f328810015af55c56660c674
PedroPabloGuzmanMayen <ppguzmanmayen@gmail.com>
2024-04-04 16:18:26 -0600

    Feat: delete method

--- src/main.js
+++ src/main.js
@@ -1,10 +1,18 @@
 import express from 'express'
 import {
-  getPosts, newPost, getPostbyID, modifyPostByID,
+  getPosts, newPost, getPostbyID, modifyPostByID, deletePost,
 } from './db.js'
+import fs from 'fs';
 
 const app = express()
 const port = 22111
+//Con esta función se guardan todas las request y response en un archivo txt (20 puntos)
+const logger = (req, res, next) => {
+  fs.appendFileSync('log.txt', `Request: ${req.method} ${req.url} Response: ${res} at ${new Date().toISOString()}\n`)
+  next()
+}
+app.use(logger)
+
 app.use(express.json())
 
 app.get('/', (req, res) => {
@@ -48,8 +56,18 @@ app.put('/modifyPost/:id', async (req, res) => {
     res.status(500).send('Error :(')
   }
 })
+app.delete('/deletePost/:id', async (req, res) => {
+  try {
+    const { id } = req.params
+    await deletePost(id)
+    res.status(200).send('Post eliminado correctamente')
+  } catch (e) {
+    res.status(500).send('Error :(')
+  }
+})
+//Verificación si el endpoint is valido (15 puntos)
 app.use((req, res, next) => {
-  res.status(404).send('404 Not Found');
+  res.status(400).send('400 bad request');
 })
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
